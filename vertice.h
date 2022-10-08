237a217acd52ab1199fe111393b63c8f9fab75d1
brutexx <79064566+brutexx@users.noreply.github.com>
2022-10-08 11:54:12 -0300

    Os tipos foram fundidos.

--- vertice.h
+++
@@ -1,9 +0,0 @@
-#ifndef VERTICE_H
-#define VERTICE_H
-
-// O tipo tVertice é dependente de tGrafo - não faz sentido
-// criá-lo sem ter um tGrafo onde colocar ele.
-
-typedef struct stVertice tVertice;
-
-#endif
