843d0d734a2f29e042332d1c9ee3aabbd2cc2327
brutexx <79064566+brutexx@users.noreply.github.com>
2022-10-12 23:40:15 -0300

    Transferi os comentários para grafo.h
    As descrições não estavam aparecendo na main.c ;-;

--- grafo.c
+++ grafo.c
@@ -38,11 +38,6 @@ static int compAresta(const void *aresta_1, const void *aresta_2);
 
 // =========== Funções do Grafo =========== //
 
-/**
- * @brief Cria um tipo tGrafo nulo
- *
- * @return tGrafo*
- */
 tGrafo *initGrafo()
 {
     tGrafo *grafo = (tGrafo *)calloc(1, sizeof(tGrafo));
@@ -53,11 +48,6 @@ tGrafo *initGrafo()
     return grafo;
 }
 
-/**
- * @brief Destrói o grafo
- *
- * @param grafo Grafo a ser liberado
- */
 void freeGrafo(tGrafo *grafo)
 {
     freeVertices(grafo);
@@ -68,13 +58,6 @@ void freeGrafo(tGrafo *grafo)
 
 // =========== Funções do Vértice =========== //
 
-/**
- * @brief Cria um tipo tVertice
- *
- * @param x Coordenada x do vértice
- * @param y Coordenada y do vértice
- * @return tVertice*
- */
 tVertice *initVertice(float x, float y)
 {
     tVertice *vertice = (tVertice *)calloc(1, sizeof(tVertice));
@@ -85,11 +68,6 @@ tVertice *initVertice(float x, float y)
     return vertice;
 }
 
-/**
- * @brief Destrói o vértice
- *
- * @param vertice Vértice a ser liberado
- */
 void freeVertice(tVertice *vertice)
 {
     free(vertice);
@@ -114,15 +92,6 @@ static void freeVertices(tGrafo *grafo)
 
 // =========== Funções da Aresta =========== //
 
-/**
- * @brief Cria um tipo tAresta conectando dois vértices
- * @details Sendo um grafo não-direcionado, ser destino ou origem não importa.
- *
- * @param grafo Grafo com o vetor de vértices
- * @param indice1 Índice do vértice origem
- * @param indice2 Índice do vértice destino
- * @return tAresta*
- */
 tAresta *initAresta(tGrafo *grafo, int indice1, int indice2)
 {
     tAresta *aresta = (tAresta *)calloc(1, sizeof(tAresta));
@@ -142,13 +111,6 @@ tAresta *initAresta(tGrafo *grafo, int indice1, int indice2)
     return aresta;
 }
 
-/**
- * @brief Cria todas as arestas possíveis do grafo
- *
- * @param grafo Grafo com os vértices
- * @pre Vetor de vértices completamente preenchido
- * @post Vetor de arestas completamente preenchido
- */
 void initAllArestas(tGrafo *grafo)
 {
     tAresta *aresta;
@@ -169,11 +131,6 @@ void initAllArestas(tGrafo *grafo)
     }
 }
 
-/**
- * @brief Destrói a aresta
- *
- * @param aresta Aresta a ser liberada
- */
 void freeAresta(tAresta *aresta)
 {
     free(aresta);
@@ -219,11 +176,6 @@ static int compAresta(const void *aresta_1, const void *aresta_2)
     return 0;
 }
 
-/**
- * @brief Organiza as arestas em ordem crescente
- *
- * @param grafo Grafo com as arestas
- */
 void sortArestas(tGrafo *grafo)
 {
     qsort(grafo->arestas, getSizeArestas(grafo), sizeof(tAresta *), compAresta);
@@ -233,14 +185,6 @@ void sortArestas(tGrafo *grafo)
 
 // ========= Getters e Setters do grafo ========= //
 
-/**
- * @brief Define quantos vértices o grafo terá. (E quantas arestas)
- *
- * @param grafo Grafo a ser modificado
- * @param size Tamanho do vetor de vértices
- * @pre Grafo não é NULL, size >= 0
- * @post Tamanho de vetor de vértices e de arestas foram ajustados
- */
 void setSizeVertices(tGrafo *grafo, int size)
 {
     grafo->sizeVertices = size;
@@ -267,25 +211,11 @@ void setSizeVertices(tGrafo *grafo, int size)
         grafo->arestas = (tAresta **)calloc(size, sizeof(tAresta *));
 }
 
-/**
- * @brief Pega a quantidade (máxima) de vértices do grafo
- * @details Quantos elementos cabem no vetor de vértices.
- *
- * @param grafo Grafo com o vetor de vértices
- * @return int
- */
 int getSizeVertices(tGrafo *grafo)
 {
     return grafo->sizeVertices;
 }
 
-/**
- * @brief Pega a quantidade (máxima) de arestas do grafo
- * @details Quantos elementos cabem no vetor de arestas.
- *
- * @param grafo Grafo com o vetor de arestas
- * @return int
- */
 int getSizeArestas(tGrafo *grafo)
 {
     int size = getSizeVertices(grafo);
@@ -303,27 +233,12 @@ int getSizeArestas(tGrafo *grafo)
 
 // ========= Getters e Setters do vértice ========= //
 
-/**
- * @brief Muda o vértice na posição do vetor indicada
- * @details Coloca o próprio índice como pai
- *
- * @param grafo Grafo com o vetor de vértices
- * @param indice Posição a ser modificada
- * @param vertice Novo vértice
- */
 void setVertice(tGrafo *grafo, int indice, tVertice *vertice)
 {
     setPai(vertice, indice);
     grafo->vertices[indice] = vertice;
 }
 
-/**
- * @brief Pega o vértice na posição do vetor indicada
- *
- * @param grafo Grafo com o vetor de vértices
- * @param indice Posição do vértice
- * @return tVertice*
- */
 tVertice *getVertice(tGrafo *grafo, int indice)
 {
     if (indice >= getSizeVertices(grafo) || indice < 0)
@@ -332,67 +247,31 @@ tVertice *getVertice(tGrafo *grafo, int indice)
     return grafo->vertices[indice];
 }
 
-/**
- * @brief Muda a coordenada x do vértice
- *
- * @param vertice Vértice a ser modificado
- * @param x Nova coordenada x
- */
 void setX(tVertice *vertice, float x)
 {
     vertice->x = x;
 }
 
-/**
- * @brief Pega a coordenada x do vértice
- *
- * @param vertice Vértice com a coordenada x
- * @return float
- */
 float getX(tVertice *vertice)
 {
     return vertice->x;
 }
 
-/**
- * @brief Muda a coordenada y do vértice
- *
- * @param vertice Vértice a ser modificado
- * @param y Nova coordenada y
- */
 void setY(tVertice *vertice, float y)
 {
     vertice->y = y;
 }
 
-/**
- * @brief Pega a coordenada y do vértice
- *
- * @param vertice Vértice com a coordenada y
- * @return float
- */
 float getY(tVertice *vertice)
 {
     return vertice->y;
 }
 
-/**
- * @brief Muda o pai do vértice
- *
- * @param vertice Vértice a ser modificado
- * @param pai Índice do novo pai
- */
 void setPai(tVertice *vertice, int pai)
 {
     vertice->pai = pai;
 }
 
-/**
- * @brief Pega o índice do pai do vértice
- *
- * @param vertice Vértice com o pai
- * @return int
- */
 int getPai(tVertice *vertice)
 {
     return vertice->pai;
@@ -400,25 +279,11 @@ int getPai(tVertice *vertice)
 
 // ========= Getters e Setters da aresta ========= //
 
-/**
- * @brief Muda a aresta na posição do vetor indicada
- *
- * @param grafo Grafo com o vetor de arestas
- * @param indice Posição a ser modificada
- * @param aresta Nova aresta
- */
 void setAresta(tGrafo *grafo, int indice, tAresta *aresta)
 {
     grafo->arestas[indice] = aresta;
 }
 
-/**
- * @brief Pega a aresta na posição do vetor indicada
- *
- * @param grafo Grafo com o vetor de arestas
- * @param indice Posição do aresta
- * @return tAresta*
- */
 tAresta *getAresta(tGrafo *grafo, int indice)
 {
     if (indice >= getSizeArestas(grafo) || indice < 0)
@@ -427,67 +292,31 @@ tAresta *getAresta(tGrafo *grafo, int indice)
     return grafo->arestas[indice];
 }
 
-/**
- * @brief Muda o vértice v1 da aresta
- *
- * @param aresta Aresta a ser modificada
- * @param v1 Índice do novo vértice v1
- */
 void setV1(tAresta *aresta, int v1)
 {
     aresta->v1 = v1;
 }
 
-/**
- * @brief Pega o índice do vértice v1 da aresta
- *
- * @param aresta Aresta com v1
- * @return int
- */
 int getV1(tAresta *aresta)
 {
     return aresta->v1;
 }
 
-/**
- * @brief Muda o vértice v2 da aresta
- *
- * @param aresta Aresta a ser modificada
- * @param v2 Índice do novo vértice v2
- */
 void setV2(tAresta *aresta, int v2)
 {
     aresta->v2 = v2;
 }
 
-/**
- * @brief Pega o índice do vértice v2 da aresta
- *
- * @param aresta Aresta com v2
- * @return int
- */
 int getV2(tAresta *aresta)
 {
     return aresta->v2;
 }
 
-/**
- * @brief Muda a distância da aresta
- *
- * @param aresta Aresta a ser modificada
- * @param dist Nova distância
- */
 void setDist(tAresta *aresta, float dist)
 {
     aresta->dist = dist;
 }
 
-/**
- * @brief Pega a distância da aresta
- *
- * @param aresta Aresta com a distância
- * @return float
- */
 float getDist(tAresta *aresta)
 {
     return aresta->dist;
--- grafo.h
+++ grafo.h
@@ -5,50 +5,241 @@ typedef struct stGrafo tGrafo;
 typedef struct stVertice tVertice;
 typedef struct stAresta tAresta;
 
-// Funções inicializadoras / liberadoras
+// Funções inicializadoras
 
+/**
+ * @brief Cria um tipo tGrafo nulo
+ *
+ * @return tGrafo*
+ */
 tGrafo *initGrafo();
+
+/**
+ * @brief Cria um tipo tVertice
+ *
+ * @param x Coordenada x do vértice
+ * @param y Coordenada y do vértice
+ * @return tVertice*
+ */
 tVertice *initVertice(float x, float y);
+
+/**
+ * @brief Cria um tipo tAresta conectando dois vértices
+ * @details Sendo um grafo não-direcionado, ser destino ou origem não importa.
+ *
+ * @param grafo Grafo com o vetor de vértices
+ * @param indice1 Índice do vértice origem
+ * @param indice2 Índice do vértice destino
+ * @return tAresta*
+ */
 tAresta *initAresta(tGrafo *grafo, int indice1, int indice2);
 
+// Funções liberadoras
+
+/**
+ * @brief Destrói o grafo
+ *
+ * @param grafo Grafo a ser liberado
+ */
 void freeGrafo(tGrafo *grafo);
+
+/**
+ * @brief Destrói o vértice
+ *
+ * @param vertice Vértice a ser liberado
+ */
 void freeVertice(tVertice *vertice);
+
+/**
+ * @brief Destrói a aresta
+ *
+ * @param aresta Aresta a ser liberada
+ */
 void freeAresta(tAresta *aresta);
 
 // Funções gerais
 
+/**
+ * @brief Cria todas as arestas possíveis do grafo
+ *
+ * @param grafo Grafo com os vértices
+ * @pre Vetor de vértices completamente preenchido
+ * @post Vetor de arestas completamente preenchido
+ */
 void initAllArestas(tGrafo *grafo);
+
+/**
+ * @brief Organiza as arestas em ordem crescente
+ *
+ * @param grafo Grafo com as arestas
+ */
 void sortArestas(tGrafo *grafo);
 
 // Funções getters e setters (Grafo)
 
+/**
+ * @brief Define quantos vértices o grafo terá. (E quantas arestas)
+ *
+ * @param grafo Grafo a ser modificado
+ * @param size Tamanho do vetor de vértices
+ * @pre Grafo não é NULL, size >= 0
+ * @post Tamanho de vetor de vértices e de arestas foram ajustados
+ */
 void setSizeVertices(tGrafo *grafo, int size);
 
+/**
+ * @brief Pega a quantidade (máxima) de vértices do grafo
+ * @details Quantos elementos cabem no vetor de vértices.
+ *
+ * @param grafo Grafo com o vetor de vértices
+ * @return int
+ */
 int getSizeVertices(tGrafo *grafo);
+
+/**
+ * @brief Pega a quantidade (máxima) de arestas do grafo
+ * @details Quantos elementos cabem no vetor de arestas.
+ *
+ * @param grafo Grafo com o vetor de arestas
+ * @return int
+ */
 int getSizeArestas(tGrafo *grafo);
 
 // Funções getters e setters (Vértice)
 
+/**
+ * @brief Muda o vértice na posição do vetor indicada
+ * @details Coloca o próprio índice como pai
+ *
+ * @param grafo Grafo com o vetor de vértices
+ * @param indice Posição a ser modificada
+ * @param vertice Novo vértice
+ */
 void setVertice(tGrafo *grafo, int indice, tVertice *vertice);
+
+/**
+ * @brief Muda a coordenada x do vértice
+ *
+ * @param vertice Vértice a ser modificado
+ * @param x Nova coordenada x
+ */
 void setX(tVertice *vertice, float x);
+
+/**
+ * @brief Muda a coordenada y do vértice
+ *
+ * @param vertice Vértice a ser modificado
+ * @param y Nova coordenada y
+ */
 void setY(tVertice *vertice, float y);
+
+/**
+ * @brief Muda o pai do vértice
+ *
+ * @param vertice Vértice a ser modificado
+ * @param pai Índice do novo pai
+ */
 void setPai(tVertice *vertice, int pai);
 
+/**
+ * @brief Pega o vértice na posição do vetor indicada
+ *
+ * @param grafo Grafo com o vetor de vértices
+ * @param indice Posição do vértice
+ * @return tVertice*
+ */
 tVertice *getVertice(tGrafo *grafo, int indice);
+
+/**
+ * @brief Pega a coordenada x do vértice
+ *
+ * @param vertice Vértice com a coordenada x
+ * @return float
+ */
 float getX(tVertice *vertice);
+
+/**
+ * @brief Pega a coordenada y do vértice
+ *
+ * @param vertice Vértice com a coordenada y
+ * @return float
+ */
 float getY(tVertice *vertice);
+
+/**
+ * @brief Pega o índice do pai do vértice
+ *
+ * @param vertice Vértice com o pai
+ * @return int
+ */
 int getPai(tVertice *vertice);
 
 // Funções getters e setters (Aresta)
 
+/**
+ * @brief Muda a aresta na posição do vetor indicada
+ *
+ * @param grafo Grafo com o vetor de arestas
+ * @param indice Posição a ser modificada
+ * @param aresta Nova aresta
+ */
 void setAresta(tGrafo *grafo, int indice, tAresta *aresta);
+
+/**
+ * @brief Muda o vértice v1 da aresta
+ *
+ * @param aresta Aresta a ser modificada
+ * @param v1 Índice do novo vértice v1
+ */
 void setV1(tAresta *aresta, int v1);
+
+/**
+ * @brief Muda o vértice v2 da aresta
+ *
+ * @param aresta Aresta a ser modificada
+ * @param v2 Índice do novo vértice v2
+ */
 void setV2(tAresta *aresta, int v2);
+
+/**
+ * @brief Muda a distância da aresta
+ *
+ * @param aresta Aresta a ser modificada
+ * @param dist Nova distância
+ */
 void setDist(tAresta *aresta, float dist);
 
+/**
+ * @brief Pega a aresta na posição do vetor indicada
+ *
+ * @param grafo Grafo com o vetor de arestas
+ * @param indice Posição do aresta
+ * @return tAresta*
+ */
 tAresta *getAresta(tGrafo *grafo, int indice);
+
+/**
+ * @brief Pega o índice do vértice v1 da aresta
+ *
+ * @param aresta Aresta com v1
+ * @return int
+ */
 int getV1(tAresta *aresta);
+
+/**
+ * @brief Pega o índice do vértice v2 da aresta
+ *
+ * @param aresta Aresta com v2
+ * @return int
+ */
 int getV2(tAresta *aresta);
+
+/**
+ * @brief Pega a distância da aresta
+ *
+ * @param aresta Aresta com a distância
+ * @return float
+ */
 float getDist(tAresta *aresta);
 
 #endif
